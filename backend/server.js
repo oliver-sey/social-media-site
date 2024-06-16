0496d0600adaab02b92b0253fe6790eb5168cd71
Oliver <84431348+oliver-sey@users.noreply.github.com>
2024-06-16 08:43:55 -0700

    Connecting to DB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
+console.log("Mongo URI: " + process.env.MONGO_URI);
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
-	console.log("In server.js, server is running...");
+app.listen(PORT, () => {
+	console.log(`In server.js, server is running on port ${PORT}...`);
+	connectMongoDB();
 });

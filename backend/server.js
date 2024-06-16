f09f177574603f49f320cf278e054de9ab4e7356
Oliver <84431348+oliver-sey@users.noreply.github.com>
2024-06-16 09:26:19 -0700

    Using express.json as middleware

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-console.log("Mongo URI: " + process.env.MONGO_URI);
+// console.log("Mongo URI: " + process.env.MONGO_URI);
+
+// Our middleware, to parse req.body
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
